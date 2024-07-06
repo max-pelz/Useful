521e8ffbe458b90381b5f0833ab538ab955ff76c
Max Pelz <maxpelz@consultdatalead.com>
2024-07-06 11:47:06 +0200

    Update bigquery__dim_dates.sql

--- bigquery__dim_dates.sql
+++ bigquery__dim_dates.sql
@@ -1,17 +1,77 @@
 -- BigQuery dates dimension table that comes in handy for e.g. Power BI implementation
-select
-    format_date('%F', d) as id,
-    d as full_date,
-    extract(year from d) as year,
-    extract(week from d) as year_week,
-    extract(day from d) as year_day,
-    extract(year from d) as fiscal_year,
-    format_date('%Q', d) as fiscal_qtr,
-    extract(month from d) as month,
-    format_date('%B', d) as month_name,
-    format_date('%w', d) as week_day,
-    format_date('%A', d) as day_name,
-    case when format_date('%A', d) IN ('Sunday', 'Saturday') then 0 else 1 end as day_is_weekday
+with 
 
-from (select * from unnest(generate_date_array('2021-07-01', '2029-12-31', interval 1 day)) as d)
-order by 1
+date_details as (
+
+    select
+        format_date('%F', d) as id,
+        d as full_date,
+        date_trunc(d, week (monday)) as week_date,
+        date_trunc(d, month) as month_date,
+        date_trunc(d, quarter) as quarter_date,
+        extract(week (monday) from d) as year_week,
+        extract(day from d) as year_day,
+        extract(year from d) as fiscal_year,
+        extract(quarter from d) as fiscal_qtr,
+        extract(month from d) as month,
+        format_date('%B', d) as month_name,
+        format_date('%A', d) as day_name,
+        not (format_date('%A', d) in ('Sunday', 'Saturday')) as day_is_weekday,
+        case
+            when format_date('%A', d) = 'Monday' then '1) monday'
+            when format_date('%A', d) = 'Tuesday' then '2) tuesday'
+            when format_date('%A', d) = 'Wednesday' then '3) wednesday'
+            when format_date('%A', d) = 'Thursday' then '4) thursday'
+            when format_date('%A', d) = 'Friday' then '5) friday'
+            when format_date('%A', d) = 'Saturday' then '6) saturday'
+            when format_date('%A', d) = 'Sunday' then '7) sunday'
+        end as weekday,
+        
+        -- custom kranus quarter start date calculation
+        case
+            when extract(month from d) < 2 then date(extract(year from d) - 1, 11, 1)
+            when extract(month from d) < 5 then date(extract(year from d), 2, 1)
+            when extract(month from d) < 8 then date(extract(year from d), 5, 1)
+            when extract(month from d) < 11 then date(extract(year from d), 8, 1)
+            else date(extract(year from d), 11, 1)
+        end as kranus_quarter_date,
+
+        -- public holidays
+        holidays.date is not null as is_holiday
+    from
+        (
+            select *
+            from
+                unnest(
+                    generate_date_array(
+                        '2021-07-01',
+                        date_sub(date_add(date_trunc(current_date(), month), interval 2 month), interval 1 day),
+                        interval 1 day
+                    )
+                ) as d
+        ) as date_series
+    left join {{ source('src_excel__public_holidays', 'public_holidays') }} as holidays
+        on date(split(holidays.date, ' ')[offset(0)]) = date_series.d
+
+)
+
+select
+    *,
+    date_add(week_date, interval 1 week) as next_week_date,
+    date_sub(quarter_date, interval 1 quarter) as last_quarter_date,
+    date_add(quarter_date, interval 1 quarter) as next_quarter_date,
+    case
+        when extract(month from full_date) <= 4 then date(extract(year from full_date) - 1, 11, 1)  -- Before May, Q1, previous Q4
+        when extract(month from full_date) <= 7 then date(extract(year from full_date), 2, 1)      -- Before August, Q2, current Q1
+        when extract(month from full_date) <= 10 then date(extract(year from full_date), 5, 1)    -- Before November, Q3, current Q2
+        else date(extract(year from full_date), 8, 1)                                           -- November and onwards, Q4, current Q3
+    end as last_kranus_quarter_date,
+    case
+        when extract(month from full_date) between 1 and 1 then date(extract(year from full_date), 2, 1)
+        when extract(month from full_date) between 2 and 4 then date(extract(year from full_date), 5, 1)
+        when extract(month from full_date) between 5 and 7 then date(extract(year from full_date), 8, 1)
+        when extract(month from full_date) between 8 and 10 then date(extract(year from full_date), 11, 1)
+        else date(extract(year from full_date) + 1, 2, 1)
+    end as next_kranus_quarter_date
+from date_details
+order by id
